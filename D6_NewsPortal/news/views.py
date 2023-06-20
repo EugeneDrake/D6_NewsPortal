1858b559ca57492e4143a7a70b5d1a99acedd4a7
EugeneDrake <119695787+EugeneDrake@users.noreply.github.com>
2023-06-20 20:34:50 +0300

    Add files via upload

---
+++ D6_NewsPortal/news/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post, Category
+
+class PostsList (ListView):
+    model = Post
+    ordering = '-created_on'
+    template_name = 'posts.html'
+    context_object_name = 'posts' #пока не понял зачем это тут надо
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
